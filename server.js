b0f5403a0757906a4bcaaed6057e216ec4f44e71
JT Kaufman <jokaufma@redhat.com>
2021-01-01 15:57:59 -0500

    Change conditional db instance logic

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
 const port = 3000;
-const db = require('./db');
-const mongodb = db(process.argv[2]);
-const routes = require('./routes');
+const db = require("./db");
+const routes = require("./routes");
 
 // Create app instance
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cors());
 
 // sets db as application wide value
-app.mongo = mongodb;
+app.mongo = db;
 
 // mounts routes to app
 routes(app);
 
 // serves app on port 3000
 app.listen(port, () => {
-  console.log('server running on port 3000')
+  console.log("server running on port 3000");
 });
+
+// allows the tests to import the app instance
+module.exports = app;
